d16fcdf8737fb2d670c4a9d1db4fd8f0732861c3
Ninos Zaiya <ninos_zaiya@hotmail.com>
2020-09-07 20:46:06 +0000

    customized admin

--- products/admin.py
+++ products/admin.py
@@ -2,9 +2,43 @@ from django.contrib import admin
 from .models import Product, Category, Occasion, Package
 # Register your models here.
 
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Occasion)
-admin.site.register(Package)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "category",
+        "occasion",
+        "package",
+        "price",
+        "rating",
+        "image",
+    )
+    ordering = ("category",)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "friendly_name",
+        "name",
+    )
+
+
+class OccasionAdmin(admin.ModelAdmin):
+    list_display = (
+        "friendly_name",
+        "name",
+    )
+
+
+class PackageAdmin(admin.ModelAdmin):
+    list_display = (
+        "friendly_name",
+        "name",
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Occasion, OccasionAdmin)
+admin.site.register(Package, PackageAdmin)
 
 
