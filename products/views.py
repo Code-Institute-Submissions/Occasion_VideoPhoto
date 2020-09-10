eba9a60c3510a94ae16d42c4e1a794741d396e53
Ninos Zaiya <ninos_zaiya@hotmail.com>
2020-09-10 20:50:25 +0000

    changed queries(package field) to fix search func.

--- products/views.py
+++ products/views.py
@@ -43,7 +43,7 @@ def all_products(request):
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
 
-            queries = Q(category__friendly_name__icontains=query) | Q(description__icontains=query) | Q(things_include__icontains=query) | Q(package__friendly_name__icontains=query) | Q(occasion__friendly_name__icontains=query)
+            queries = Q(category__friendly_name__icontains=query) | Q(description__icontains=query) | Q(things_include__icontains=query) | Q(package__name__icontains=query) | Q(occasion__friendly_name__icontains=query)
             products = products.filter(queries)
     current_sorting = f'{sort}_{direction}'
 
