078cab73e8d130cfc6312513cfa74e328a0aaca4
Ninos Zaiya <ninos_zaiya@hotmail.com>
2020-09-12 10:12:41 +0000

    added total quantity price to the bag page

--- bag/contexts.py
+++ bag/contexts.py
@@ -9,18 +9,21 @@ def bag_contects(request):
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
-
+    
     for item_id, quantity in bag.items():
         product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
+        new_prod_price = product.price * Decimal(settings.PRECENT_ADVANCE_PAYMENT_OF_FULL_PRICE) ## to get 30% av orginal price
+        prod_qty = Decimal(quantity * new_prod_price)
+        total += quantity * new_prod_price
         product_count += quantity
         bag_items.append({
             'item_id': item_id,
             'quantity': quantity,
             'product': product,
+            'prod_quat': prod_qty,
         })
 
-    grand_total = total * Decimal(settings.PRECENT_ADVANCE_PAYMENT_OF_FULL_PRICE)  ## 30%
+    grand_total = total
     context = {
         'bag_items': bag_items,
         'total': total,
