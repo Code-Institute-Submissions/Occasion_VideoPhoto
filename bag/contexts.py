907babaff0bce2dbc81a188a61fcf04709e86088
Ninos Zaiya <ninos_zaiya@hotmail.com>
2020-09-13 21:23:50 +0000

    Added remove and update func. to bag page

--- bag/contexts.py
+++ bag/contexts.py
@@ -13,14 +13,14 @@ def bag_contects(request):
     for item_id, quantity in bag.items():
         product = get_object_or_404(Product, pk=item_id)
         new_prod_price = product.price * Decimal(settings.PRECENT_ADVANCE_PAYMENT_OF_FULL_PRICE) ## to get 30% av orginal price
-        prod_qty = Decimal(quantity * new_prod_price)
+        total_prod_price = Decimal(quantity * new_prod_price)
         total += quantity * new_prod_price
         product_count += quantity
         bag_items.append({
             'item_id': item_id,
             'quantity': quantity,
             'product': product,
-            'prod_qty': prod_qty,
+            'total_prod_price': total_prod_price,
         })
 
     grand_total = total
