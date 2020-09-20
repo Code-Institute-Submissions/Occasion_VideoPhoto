b21cf40b5ee5a85c79affa338e1627797ede5893
Ninos Zaiya <ninos_zaiya@hotmail.com>
2020-09-20 20:42:39 +0000

    Added profile app

--- checkout/models.py
+++ checkout/models.py
@@ -6,10 +6,13 @@ from products.models import Product, Occasion, Package, Category
 from django.conf import settings
 from decimal import Decimal
 from django_countries.fields import CountryField
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders') # add a related name of orders so we can access the users orders by calling something like user.userprofile.orders
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
