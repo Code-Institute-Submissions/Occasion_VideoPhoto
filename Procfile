7856d6a257c0d3eec12da22b8daf5810ed401a42
Ninos Zaiya <ninos_zaiya@hotmail.com>
2020-09-22 14:43:02 +0000

    deployment to heruko part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn occasion_Video_photo.wsgi:application
--- occasion_Video_photo/settings.py
+++ occasion_Video_photo/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '20y=f@*!vg*9^v=m&5tso0z1vvgocwfh(!ol%^dxti#uy&#k1+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['occasion-video-photo.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,12 +123,17 @@ WSGI_APPLICATION = 'occasion_Video_photo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
