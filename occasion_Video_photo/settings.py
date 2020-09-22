6dc0c49a42c6b34d5e2b683330df82f58e7d6d31
Ninos Zaiya <ninos_zaiya@hotmail.com>
2020-09-22 14:54:34 +0000

    Removed secret key and set debug

--- occasion_Video_photo/settings.py
+++ occasion_Video_photo/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '20y=f@*!vg*9^v=m&5tso0z1vvgocwfh(!ol%^dxti#uy&#k1+'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['occasion-video-photo.herokuapp.com', 'localhost']
 
