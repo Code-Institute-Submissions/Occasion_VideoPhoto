d72c2dfa4e57f8d6f8ff2a6dcd437c1ad40e5017
Ninos Zaiya <ninos_zaiya@hotmail.com>
2020-09-23 19:11:13 +0000

    show friendly name for all fields in product managment

--- products/forms.py
+++ products/forms.py
@@ -14,10 +14,20 @@ class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs): # override the init method to make a couple changes to the fields.
         super().__init__(*args, **kwargs) 
 
-        categories = Category.objects.all()# list comprehension with shorthand way for loop that adds items to a list.
-        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+        categories = Category.objects.all() # list comprehension with shorthand way for loop that adds items to a list.
+        friendly_names_category = [(c.id, c.get_friendly_name()) for c in categories]
 
-        self.fields['category'].choices = friendly_names
+        occasions = Occasion.objects.all() # list comprehension with shorthand way for loop that adds items to a list.
+        friendly_names_occasion = [(c.id, c.get_friendly_name()) for c in occasions]
+
+        packages = Package.objects.all() # list comprehension with shorthand way for loop that adds items to a list.
+        friendly_names_package = [(c.id, c.get_friendly_name()) for c in packages]
+
+        self.fields['category'].choices = friendly_names_category
+        self.fields['occasion'].choices = friendly_names_occasion
+        self.fields['package'].choices = friendly_names_package
+
+
 
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
