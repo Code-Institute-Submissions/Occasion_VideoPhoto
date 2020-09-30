fd9e2fc27f6cbc1d33de8fcf98cb453c7874d04a
Ninos Zaiya <ninos_zaiya@hotmail.com>
2020-09-30 20:12:42 +0000

    Added test func. to test add to bag and get bag list

--- bag/contexts.py
+++ bag/contexts.py
@@ -9,7 +9,7 @@ def bag_contects(request):
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
-    
+
     for item_id, quantity in bag.items():
         product = get_object_or_404(Product, pk=item_id)
         new_prod_price = product.price * Decimal(settings.PRECENT_ADVANCE_PAYMENT_OF_FULL_PRICE) ## to get 30% av orginal price
--- bag/tests.py
+++ bag/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from products.models import Product, Package, Occasion
 
 # Create your tests here.
+
+
+class TestViews(TestCase):
+
+    def test_get_bag_list(self):
+        response = self.client.get('/bag/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bag/bag.html')
+
+    def test_add_item_pag(self):
+        id = 1
+        response = self.client.post(f'/bag/add/ {id}')
+        self.assertEqual(response.status_code, 301)
