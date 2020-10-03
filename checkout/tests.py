b88d921bcc53656dafa66add7bdfa7de993eceda
Ninos Zaiya <ninos_zaiya@hotmail.com>
2020-10-03 21:06:25 +0000

    Write more test(forms,models)

--- checkout/tests.py
+++ checkout/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .forms import OrderForm
 
-# Create your tests here.
+
+class TestCheckoutForm(TestCase):
+
+    def test_checkout_fullName_is_required(self):
+        form = OrderForm({'full_name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('full_name', form.errors.keys())
+        self.assertEqual(form.errors['full_name'][0], 'This field is required.')
+
+    def test_street_address2_field_is_not_required(self):
+        form = OrderForm({'full_name': 'Test','email':'test@hotmail.com',
+                          'phone_number':'123','street_address1':'test',
+                          'town_or_city':'test','country':'AA'})
+        self.assertFalse(form.is_valid())
--- products/tests.py
+++ products/tests.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
-from products.models import Product
+from products.models import Product, Category
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.test.client import Client
-
+from .forms import ProductForm
 # Create your tests here.
 
 
@@ -28,7 +28,22 @@ class TestViews(TestCase):
     def test_edit_product(self):
         product = Product.objects.create(description="test",
                                          things_include="test1", price=500)
-        response = self.client.post(f'/products/edit/{product.id}/', {'description': 'updated description'} )
+        response = self.client.post(f'/products/edit/{product.id}/',
+                                    {'description': 'updated description'})
         self.assertRedirects(response, f'/accounts/login/?next=/products/edit/{product.id}/')
         updated_product = Product.objects.get(id=product.id)
         self.assertEqual(updated_product.description, 'test')
+
+    def test_add_product(self):
+        product = Product.objects.create(description="test",
+                                         things_include="test1", price=500)
+        response = self.client.post(f'/products/edit/{product.id}/')
+        self.assertRedirects(response, f'/accounts/login/?next=/products/edit/{product.id}/')
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = ProductForm()
+        self.assertEqual(form.Meta.fields, '__all__')
+
+    def test_category_string_method_returns_name(self):
+        category = Category.objects.create(name='Test Category')
+        self.assertEqual(str(category), 'Test Category')    
